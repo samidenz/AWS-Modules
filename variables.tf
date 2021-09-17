571e60f03d832cb5211e60d9106f97e0921e656f
samidenz <87256370+samidenz@users.noreply.github.com>
2021-09-17 16:11:20 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,16 +1,14 @@
 variable "aws_region" {
-  description = "The AWS region to create things in."
-  default     = "ap-south-1"
+  default = "ap-south-1"
 }
-
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "Jenkins"
+  default     = "Jenkin_Keypair"
 }
 
 variable "instance_type" {
   description = "instance type for ec2"
-  default     =  "t2.micro"
+  default     = "t2.micro"
 }
 
 variable "security_group" {
