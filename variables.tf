bcd99686748b29cd94af2e0132ee5868d0bc236f
samidenz <87256370+samidenz@users.noreply.github.com>
2021-09-26 13:53:51 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,7 +3,7 @@ variable "aws_region" {
 }
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     = "Jenkin_Keypair"
+  default     = "Jenkins_Keypair"
 }
 
 variable "instance_type" {
